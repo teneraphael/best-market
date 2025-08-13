03a088e8227a1853245d4f403665eddc1a7b10cf
teneraphael <teneraphael57@gmail.com>
2025-08-13 03:14:30 +0100

    10-create-browsing-history

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,7 +6,7 @@
    import { Card } from '@/components/ui/card'
    import { CardContent } from '@/components/ui/card'
    import ProductSlider from '@/components/shared/product/product-slider'
-
+   import BrowsingHistoryList from '@/components/shared/browsing-history-list'
    
 
 
@@ -87,6 +87,9 @@
        </CardContent>
      </Card>
          </div>
+          <div className='p-4 bg-background'>
+             <BrowsingHistoryList />
+          </div>
        </>
      )
    }
