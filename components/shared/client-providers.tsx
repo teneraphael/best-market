858a5403ce2d72cf0a044266cd8b0805b1cd1dba
teneraphael <teneraphael57@gmail.com>
2025-08-19 05:38:38 +0100

    26-add-theme-color

--- components/shared/client-providers.tsx
+++ components/shared/client-providers.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import useCartSidebar from '@/hooks/use-cart-sidebar'
 import CartSidebar from './cart-sidebar'
 import { Toaster } from '../ui/toaster'
+import { ThemeProvider } from './theme-provider'
 
 export default function ClientProviders({
   children,
@@ -12,16 +13,16 @@ export default function ClientProviders({
   const isCartSidebarOpen = useCartSidebar()
 
   return (
-    <>
-      {isCartSidebarOpen ? (
-        <div className='flex min-h-screen'>
-          <div className='flex-1 overflow-hidden'>{children}</div>
-          <CartSidebar />
-        </div>
-      ) : (
-        <div>{children}</div>
-      )}
-      <Toaster />
-    </>
+      <ThemeProvider attribute='class' defaultTheme='system'>
+        {isCartSidebarOpen ? (
+          <div className='flex min-h-screen'>
+            <div className='flex-1 overflow-hidden'>{children}</div>
+            <CartSidebar />
+          </div>
+        ) : (
+          <div>{children}</div>
+        )}
+        <Toaster />
+      </ThemeProvider>
   )
 }
--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,16 +1,42 @@
- import CartButton from './cart-button'
- import UserButton from './user-button'
+import CartButton from './cart-button'
+import { EllipsisVertical } from 'lucide-react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import ThemeSwitcher from '../theme-switcher'
+import UserButton from './user-button'
 
-   export default function Menu() {
-     return (
-       <div className='flex justify-end'>
-         <nav className='flex gap-3 w-full'>
-           <UserButton />
-
-          
+export default function Menu() {
+  return (
+    <div className='flex justify-end'>
+      <nav className='md:flex gap-3 hidden w-full'>
+        <ThemeSwitcher />
+        <UserButton />
+        <CartButton />
+      </nav>
+      <nav className='md:hidden'>
+        <Sheet>
+          <SheetTrigger className='align-middle header-button'>
+            <EllipsisVertical className='h-6 w-6' />
+          </SheetTrigger>
+          <SheetContent className=' bg-black text-white  flex flex-col items-start  '>
+            <SheetHeader className='w-full'>
+              <div className='flex items-center justify-between '>
+                <SheetTitle>Site Menu</SheetTitle>
+                <SheetDescription></SheetDescription>
+              </div>
+            </SheetHeader>
+            <ThemeSwitcher />
+            <UserButton />
             <CartButton />
-       
-         </nav>
-       </div>
-     )
-   }
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </div>
+  )
+}
